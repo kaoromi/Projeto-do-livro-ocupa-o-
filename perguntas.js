c4562226f427e3c11aa73389c31452a9b8db4f5e
kaoromi <nicoleunderchara@gmail.com>
2024-11-18 19:33:15 -0300

    Flashcards completos

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Tema Central',
+    'Qual é o tema central do livro "Ocupação"?',
+    'O tema central do livro é a luta dos jovens por seus direitos e por uma educação de qualidade, retratando a ocupação de uma escola pública por estudantes como forma de protesto.'
+)
+
+criaCartao(
+    'Personagem',
+    'Quem é o protagonista da história?',
+    'O protagonista é Caio.'
+)
+
+criaCartao(
+    'Relações e Conflitos',
+    'Como o livro aborda a relação entre os jovens e os adultos?',
+    'O livro destaca o conflito geracional, mostrando o embate entre as visões dos jovens, que desejam mudanças, e as dos adultos, que nem sempre compreendem ou apoiam suas ações.'
+)
+
+criaCartao(
+    'Reflexão',
+    'Qual é a mensagem principal que o livro busca transmitir?',
+    'A obra transmite a importância do engajamento social, da solidariedade e da resistência diante das dificuldades, destacando o papel dos jovens como agentes de transformação na sociedade.'
+)
